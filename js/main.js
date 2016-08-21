42007af48fb4fee43cb914f9f433edeaa67ecfc8
jsnhff <jsnhff@gmail.com>
2016-08-21 15:56:07 -0400

    Final moves for animating story

--- js/main.js
+++ js/main.js
@@ -8,23 +8,44 @@ var total_height = 0;
 var previous_height = 0;
 var previous_height_count = 0;
 var single_word_height = 0;
+var tota_word_count = 0;
 
 // Get book from text file
-$.ajax({
-    url : "/books/the_notebook.txt",
-    dataType: "text",
-    success : function (data) {
-        book = data.replace(line_break, "<br>"); // Replace linebreaks
-        book = book.replace(double_break, "<br> "); // Replace linebreaks
-        book = book.split(' '); // Split on space
-        write(book);
-    }
-});
+function loadBook() {
+    console.log("load book");
+    // Show the wrapper and reset vars to default states
+    $("#wrapper").show();
+    height_count = 0;
+    top_margin = 0;
+    total_height = 0;
+    previous_height = 0;
+    previous_height_count = 0;
+    total_word_count = 0;
+
+    // Choose a random book
+    // Choose corresonponding image folder
+    // Load random images
+
+    $.ajax({
+        url : "/books/the_notebook.txt",
+        dataType: "text",
+        success : function (data) {
+            book = data.replace(line_break, "<br>"); // Replace linebreaks
+            book = book.replace(double_break, "<br> "); // Replace linebreaks
+            book = book.split(' '); // Split on space
+            write(book);
+        }
+    });
+}
 
 // Add words from book to HTML, keeping them hidden
 function write(book){
+    // Clear writing space
+    home.html('');
+
+    // Add the words
     for (i = 0; i < book.length; i++) {
-        var word_wrapped = " <span class='word inline-block' style='display:none;'>"+book[i]+"</span>"; 
+        var word_wrapped = " <span class='word' style='display:none;'>"+book[i]+"</span>"; 
         home.append(word_wrapped);
     }
 
@@ -35,13 +56,14 @@ function write(book){
 
 // Fade each word in slowly, one after the other
 function showWords() {
+    total_word_count = $(".word").length;
     $(".word").each(function(index, element){
         var text = $(element).html();
         var delay = index*1500;
 
         $(element).delay(delay).fadeIn(1000, function(){
             total_height = $("#js-romance").height();
-            single_word_height = $(".word").height();
+            single_word_height = 68; // 68 because using inline-block removes brs
 
             if(height_count == 0) {
                 height_count = 1;
@@ -59,9 +81,26 @@ function showWords() {
                     $("#js-romance").css("margin-top",top_margin+"px");
                 }
             } else {
-                console.log("Same height or less than 3");
+                //console.log("Same height or less than 3");
+            }
+            // Fade everything out after you reach the final word
+            if (index == total_word_count-1) {
+                $("#wrapper").fadeOut(3000, function(){
+                    home.css("margin-top","0"); 
+
+                    // Hide all the words that were faded in
+                    $(".word").each(function(){
+                        $(this).hide(); 
+                    });
+
+                    // Start it all over
+                    console.log("start over");
+                    loadBook();
+                });
             }
         });
+
     });
 }
 
+loadBook();
